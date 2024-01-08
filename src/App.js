cedbc10515bcd9209ab8e4a0c159fbd3198708a6
GayeBuse <gayebuseozkan@gmail.com>
2024-01-08 19:23:52 +0300

    BrowserRouter-Switch-page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from "react";
 import "./App.css";
+import { Route, Switch } from "react-router-dom/cjs/react-router-dom.min";
+import Home from "./Pages/Home";
+import OrderForm from "./Pages/OrderForm";
+import Success from "./Pages/Success";
+
+
 const App = () => {
   return (
     <>
+    <div className="app">
+      <Switch>
+        <Route exact path="/">
+          < Home />
+        </Route>
+        <Route exact path="/pizza">
+          < OrderForm />
+        </Route>
+        <Route exact path="/success">
+          < Success />
+        </Route>
+      </Switch>
+    </div>
    
-      <h1>Teknolojik Yemekler</h1>
-      <p>Burdaki kodu silip kendi headerınızı ekleyebilirsiniz</p>
     </>
   );
 };
---
+++ src/Pages/OrderForm.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function OrderForm() {
+    return <div>
+        sipariş sayfası...
+    </div>;
+}
