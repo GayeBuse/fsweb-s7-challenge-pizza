7ee16b675be829d651c4b06099f608cb9c46770f
GayeBuse <gayebuseozkan@gmail.com>
2024-01-08 14:36:27 +0300

    yenii

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+const App = () => {
+  return (
+    <>
+   
+      <h1>Teknolojik Yemekler</h1>
+      <p>Burdaki kodu silip kendi headerınızı ekleyebilirsiniz</p>
+    </>
+  );
+};
+export default App;
