18c5e256dc6f15c2d399fb3202f4d75855bcbb6f
GayeBuse <152473021+GayeBuse@users.noreply.github.com>
2024-03-16 17:51:11 +0000

    Pending changes exported from your codespace

---
+++ build/precache-manifest.655b3fe9a63c0d20b641b65c0a1d52ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4f997b38b6707ffb5630",
+    "url": "/static/css/main.a227fb80.chunk.css"
+  },
+  {
+    "revision": "4f997b38b6707ffb5630",
+    "url": "/static/js/main.aba3862b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "835a3d597d397c1615c3",
+    "url": "/static/js/2.7d09c7a1.chunk.js"
+  },
+  {
+    "revision": "e1143cc0e3fcb007c841e793e7c7047f",
+    "url": "/static/media/mvp-banner.e1143cc0.png"
+  },
+  {
+    "revision": "64117979e18f0a9da85b44a9f68e9afb",
+    "url": "/index.html"
+  }
+];
