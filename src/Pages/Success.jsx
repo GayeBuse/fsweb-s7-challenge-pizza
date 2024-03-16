2e0636b8e5d5ef200429aad5e3d8a435dfa9824e
GayeBuse <gayebuseozkan@gmail.com>
2024-03-16 16:53:02 +0300

    success revize

--- src/Pages/Success.jsx
+++ src/Pages/Success.jsx
@@ -1,18 +1,21 @@
-import React from 'react';
+import React from "react";
 import "./Success.css";
 
-export default function Success({order}) {
-    return <div className="success">
-    <h1>Teknolojik Yemekler</h1>
-    <div className="text">
-      <p>SİPARİŞ ALINDI</p>
-      <p></p>
-        <div className='orderozet'> 
+export default function Success({ order }) {
+  return (
+    <div className="success">
+      <h1>Teknolojik Yemekler</h1>
+      <div className="text">
+        <p className="siparis">SİPARİŞ ALINDI</p>
+        <p></p>
+        <div className="orderozet">
           <p>Boyut: {order.size}</p>
           <p>Hamur: {order.dough}</p>
-          <p>Ek Malzemeler: {order.extraIngredient}</p><br></br>
+          <p>Ek Malzemeler: {order.extraIngredient}</p>
+          <br></br>
           <p>Toplam Fiyat: {order.totalPrice}₺</p>
         </div>
+      </div>
     </div>
-  </div>
+  );
 }
