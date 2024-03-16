4ffe89f085ed94244b73607b14079b95645b3908
GayeBuse <gayebuseozkan@gmail.com>
2024-03-16 16:53:09 +0300

    .

--- src/Layout/Header.jsx
+++ src/Layout/Header.jsx
@@ -1,18 +1,14 @@
 import React from "react";
 import "./Header.css";
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <div className="home-container">
       <header>
         <h1>Teknolojik Yemekler</h1>
         <nav className="navbar">
-          <Link to= "/">Anasayfa -</Link>
-            <b>Siparis Olustur</b>k
-            
- <div>
-  jsjsjs
-  </div>       
+          <Link to="/">Anasayfa -</Link>
+          <b>Siparis Olustur</b>
         </nav>
       </header>
     </div>
